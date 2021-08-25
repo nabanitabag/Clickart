d43422ef88b12932ababf0a8995358b8f2864fbf
Nabanita Bag <ashley.makruz@gmail.com>
2021-08-25 20:22:21 +0530

    Delete setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++
@@ -1,2 +0,0 @@
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
