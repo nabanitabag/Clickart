ae1c0200dcceac486a7ebaed654ad30694194439
Nabanita Bag <ashley.makruz@gmail.com>
2021-08-25 20:30:21 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
-// Find configuration options listed here - https://jestjs.io/docs/en/configuration
+//https://jestjs.io/docs/en/configuration
 module.exports = {
   testEnvironment: "node",
-  // Overwrite the "NODE_ENV" variable when running tests
   testEnvironmentOptions: {
     NODE_ENV: "test",
   },
